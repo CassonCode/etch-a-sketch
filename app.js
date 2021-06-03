89b4d8fefdb32222f5bd948caf65afe88dbcd19e
Ben Casson <bcasson11@gmail.com>
2021-06-03 18:22:09 -0500

    Create function to generate grid with any number of elements

--- app.js
+++ app.js
@@ -3,5 +3,30 @@
 
 // const barHeight = elementHeight - actualHeight;
 
-const bodyContainer = document.querySelector(".body-container");
+const bodyContainer = document.querySelector(".container-body");
+const clearButtonContainer = document.querySelector(".container-clear-button");
+const gridContainer = document.querySelector(".container-grid");
+const header = document.querySelector("header");
+
+
 bodyContainer.style.height = window.innerHeight;
+// clearButtonContainer.style.height = bodyContainer.clientHeight - gridContainer.clientHeight - header.clientHeight;
+
+
+// function makeInitialGrid() {
+//     makeGrid(16);
+// }
+
+// makeInitialGrid();
+
+
+function makeGrid(gridSize) {
+    gridContainer.style.gridTemplateColumns = "repeat(" + gridSize + ", 1fr)";
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const newGridDiv = document.createElement("div");
+        newGridDiv.classList = "grid-pieces";
+        gridContainer.appendChild(newGridDiv);
+    }
+}
+
+window.addEventListener("load", makeGrid(16));
