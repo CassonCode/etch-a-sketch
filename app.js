ae9e45fcac64ab3e3844fa1a2352688eb76125ce
Ben Casson <bcasson11@gmail.com>
2021-06-03 14:26:41 -0500

    Adjust content container size based on mobile browser bar height

---
+++ app.js
@@ -0,0 +1,7 @@
+// const actualHeight = window.innerHeight;
+// const elementHeight = document.getElementById('control-height').clientHeight;
+
+// const barHeight = elementHeight - actualHeight;
+
+const bodyContainer = document.querySelector(".body-container");
+bodyContainer.style.height = window.innerHeight;
