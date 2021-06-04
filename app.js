311055f90f2648924e22f8c681576768f22d28ff
Ben Casson <bcasson11@gmail.com>
2021-06-03 22:16:13 -0500

    Add menu transition and close menu icon

--- app.js
+++ app.js
@@ -8,18 +8,12 @@ const clearButtonContainer = document.querySelector(".container-clear-button");
 const gridContainer = document.querySelector(".container-grid");
 const header = document.querySelector("header");
 const menu = document.querySelector(".menu");
+const hamburgerIcon = document.querySelector(".hamburger-button");
+const xHamburgerIcon = document.querySelector(".x-hamburger-button");
 
 bodyContainer.style.height = window.innerHeight;
 menu.style.height = window.innerHeight + "px";
 // clearButtonContainer.style.height = bodyContainer.clientHeight - gridContainer.clientHeight - header.clientHeight;
-
-
-// function makeInitialGrid() {
-//     makeGrid(16);
-// }
-
-// makeInitialGrid();
-
 
 function makeGrid(gridSize) {
     gridContainer.style.gridTemplateColumns = "repeat(" + gridSize + ", 1fr)";
@@ -31,3 +25,18 @@ function makeGrid(gridSize) {
 }
 
 window.addEventListener("load", makeGrid(16));
+
+function displayMenu() {
+        menu.classList.add("showMenu");
+        menu.classList.remove("hideMenu");
+        return;
+}
+
+function closeMenu() {
+    menu.classList.add("hideMenu");
+    menu.classList.remove("showMenu");
+    return;
+}
+
+hamburgerIcon.addEventListener("click", displayMenu);
+xHamburgerIcon.addEventListener("click", closeMenu);
