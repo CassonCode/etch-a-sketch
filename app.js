59f968241f0362054d7fede81696af1007b8b826
Ben Casson <bcasson11@gmail.com>
2021-06-06 16:05:55 -0500

    Disable faulty fade to black functionality

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ const clearButton = document.querySelector(".clear-button");
 
 let colorChoiceColorWheel = "on";
 let colorChoiceRainbow = "off";
+let isSelectedFadeToBlack = "off";
+
+colorInputColorWheel.value = "rgb(0,0,0)";
+
 
 function selectColorWheel() {
     colorInputRainbowButton.style.boxShadow = "none";
@@ -42,15 +46,17 @@ function selectRainbowButton() {
 }
 
 function selectFadeToBlack() {
-    if (fadeToBlackButton.style.color === "black") {
+    if (isSelectedFadeToBlack === "off") {
         fadeToBlackButton.style.boxShadow = "0 0 6px 3px white";
         fadeToBlackButton.style.border = "3px solid white";
         fadeToBlackButton.style.color = "white";
+        isSelectedFadeToBlack = "on";
     }
     else {
         fadeToBlackButton.style.boxShadow = "none";
         fadeToBlackButton.style.border = "3px solid black";
         fadeToBlackButton.style.color = "black";
+        isSelectedFadeToBlack = "off";
     }
 }
 
@@ -60,22 +66,76 @@ menu.style.height = window.innerHeight + "px";
 menuOptionsContainer.style.height = ((window.innerHeight) - (0.1 * window.innerWidth) - (0.165 * window.innerHeight)) + "px";
 
 
+// function makePixelDarker(tempColor) {
+//     // let tempColor = this.style.backgroundColor;
+//     if (tempColor[tempColor.length - 2] === "1") {
+//         let slicedColor = tempColor.slice(0, -2);
+//         slicedColor += "0.9)";
+//         tempColor = slicedColor;
+//         return tempColor;
+//     }
+//     else {
+//         let slicedAValueUnder1 = parseInt(tempColor.slice(-4));
+//         let slicedColorUnder1 = tempColor.slice(0, -4);
+//         if (slicedAValueUnder1 >= 0.1) {
+//             slicedAValueUnder1 -= 0.1;
+//             slicedColorUnder1 += slicedAValueUnder1 + ")";
+//             tempColor = slicedColorUnder1;
+//             return tempColor;
+//         }
+//     }
+// }
+
+function colorPixel() {
+    // if (isSelectedFadeToBlack === "off") {
+        if (colorChoiceRainbow === "on") {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+            console.log(this.style.backgroundColor);
+        }
+        else {
+            // this.style.backgroundColor = colorInputColorWheel.value + "";
+            // let color = colorInputColorWheel.value + "";
+            // new_col = color.replace(/rgb/i, "rgba");
+            // new_col = new_col.replace(/\)/i,',1)');
+            // this.style.backgroundColor = new_col + "";
+            this.style.backgroundColor = colorInputColorWheel.value + "";
+            console.log(this.style.backgroundColor);
+        }
+    // }
+    // else {
+    //     if (colorChoiceRainbow === "on" && (this.style.backgroundColor === "rgba(255,255,255,1)" || this.style.backgroundColor === "rgb(255, 255, 255)")) {
+    //         let color = `rgba(${Math.random() * 254},${Math.random() * 254},${Math.random() * 254},1)`;
+    //         this.style.backgroundColor = color;
+    //         console.log(this.style.backgroundColor);
+    //     }
+    //     else if (colorChoiceRainbow === "on" && (this.style.backgroundColor !== "rgba(255,255,255,1)" || this.style.backgroundColor !== "rgb(255, 255, 255)")) {
+    //         // let color = ;
+    //         // // new_col = color.replace(/rgb/i, "rgba");
+    //         // // new_col = new_col.replace(/\)/i,',0.75)');
+    //         // // this.style.backgroundColor = new_col + "";
+    //         // this.style.backgroundColor = color + "";
+    //         makePixelDarker(this.style.backgroundColor);
+    //         console.log(this.style.backgroundColor);
+    //     }
+    //     else if (colorChoiceRainbow === "off" && (this.style.backgroundColor === "rgba(255,255,255,1)" || this.style.backgroundColor === "rgb(255, 255, 255)") && colorInputColorWheel.value !== "rgb(255, 255, 255") {
+    //         let color = colorInputColorWheel.value + "";
+    //         new_col = color.replace(/rgb/i, "rgba");
+    //         new_col = new_col.replace(/\)/i,',1)');
+    //         this.style.backgroundColor = new_col + "";
+    //         console.log(this.style.backgroundColor);
+    //     }
+    //     else  {
+    //         makePixelDarker(this.style.backgroundColor);
+    //         console.log(this.style.backgroundColor);
+    //     }
+    // }
+}
+
 function resetGrid(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
-
-
-function colorPixel() {
-    if (colorChoiceRainbow === "on") {
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    }
-    else {
-        this.style.backgroundColor = colorInputColorWheel.value + "";
-    }
-}
-
 
 function makeGrid(gridSize) {
     resetGrid(gridContainer);
@@ -85,12 +145,7 @@ function makeGrid(gridSize) {
         const newGridDiv = document.createElement("div");
         newGridDiv.classList = "grid-pieces";
         gridContainer.appendChild(newGridDiv);
-        // if (gridLinesButton.classList.contains("grid-lines-on")) {
-        //     newGridDiv.style.border = "1px solid black";
-        // }
-        // else {
-        //     newGridDiv.style.border = "none";
-        // }
+        newGridDiv.style.backgroundColor = "rgba(255,255,255,1)";
     }
     let gridPixels = gridContainer.querySelectorAll('div');
     gridPixels.forEach(gridPixel => gridPixel.addEventListener('click', colorPixel));
@@ -129,10 +184,6 @@ function selectGridLines() {
         gridLinesButton.style.border = "3px solid white";
         gridLinesButton.style.boxShadow = "0 0 6px 3px white";
     }
-    // makeGrid(userBoxesNumber);
-    // for (let i = 0; i < gridContainer.children.length; i++) {
-    //     gridContainer.children[i].style.;
-    //   }
     if (gridLinesButton.classList.contains("grid-lines-on")) {
         for (let i = 0; i < gridContainer.children.length; i++) {
             gridContainer.children[i].style.border = "1px solid black";
